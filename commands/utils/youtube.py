2a7086fe03b7f23bfaa1bbcedb953cfc562fe9dd
cleviojr <cleviojr@DESKTOP-F5RVNBT.localdomain>
2018-03-09 00:37:08 +0000

    add playlist search by id

--- commands/utils/youtube.py
+++ commands/utils/youtube.py
@@ -2,14 +2,16 @@ from googleapiclient.discovery import build
 from config import CONFIG
 
 service = build('youtube', 'v3', developerKey=CONFIG['YOUTUBE_API_KEY'])
+
 class Youtube():
     def __init__(self):
         pass
         
     def search(self, query):
-        resultado = service.search().list(type='video', part='id,snippet', q=query, maxResults=1).execute()['items'][0]
-        print(f"musica encontrada: {resultado['snippet']['title']}")
-        return resultado['id']['videoId']
+        result = service.search().list(type='video', part='id,snippet', q=query, maxResults=1).execute()['items'][0]
+        print(f"found a song: {result['snippet']['title']}")
+        return result['id']['videoId']
     
-    def list_playlist_videos(self, id):
-        pass
+    def list_playlist_videos(self, playlist_id):
+        result = service.playlistItems().list(part='contentDetails', playlistId=playlist_id, maxResults=50).execute()['items'] 
+        return [item['contentDetails']['videoId'] for item in result]
