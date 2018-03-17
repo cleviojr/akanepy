0fe6f550dc662de5a9aa36b12517168877d26923
cleviojr <cleviojr20@gmail.com>
2018-03-16 22:38:51 -0300

    add !setmusic

---
+++ commands/music/setmusic.py
@@ -0,0 +1,29 @@
+from utils.embeds import send_executed_embed
+
+class SetMusicCmd():
+    def __init__(self, client, manager):
+        self.client  = client
+        self.manager = manager
+
+    async def run(self, message, args):
+        server_id = message.server.id
+        channel   = message.channel
+        try:
+            if args:
+                if args[0] == 'mute':
+                    self.manager.server_managers[f'{server_id}'].textch = None
+
+                    await send_executed_embed(self.client, message,
+                                              command='setmusic',
+                                              text='I won\'t send any'
+                                                   'music command reply.')
+            else:
+                self.manager.server_managers[f'{server_id}'].textch = channel
+
+                await send_executed_embed(self.client, message,
+                                          command='setmusic',
+                                          text='The channel I will be replying'
+                                               'music commands is now'
+                                               f'{channel.mention}.')
+        except KeyError:
+            pass
