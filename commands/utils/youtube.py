5e05adb8c0b40b68ad7612acbdd658784074a52c
cleviojr <cleviojr20@gmail.com>
2018-03-16 22:44:57 -0300

    fix PEP8, add snippet to search method

--- commands/utils/youtube.py
+++ commands/utils/youtube.py
@@ -1,17 +1,26 @@
 from googleapiclient.discovery import build
 from config import CONFIG
 
-service = build('youtube', 'v3', developerKey=CONFIG['YOUTUBE_API_KEY'])
+service = build('youtube', 'v3', developerKey=CONFIG['youtube_key'])
+
 
 class Youtube():
     def __init__(self):
         pass
-        
+
     def search(self, query):
-        result = service.search().list(type='video', part='id,snippet', q=query, maxResults=1).execute()['items'][0]
-        print(f"found a song: {result['snippet']['title']}")
-        return result['id']['videoId']
-    
+        result = service.search().list(
+                                       type='video',
+                                       part='id,snippet',
+                                       q=query,
+                                       maxResults=1
+                                      ).execute()['items'][0]
+        return result['id']['videoId'], result['snippet']['title']
+
     def list_playlist_videos(self, playlist_id):
-        result = service.playlistItems().list(part='contentDetails', playlistId=playlist_id, maxResults=50).execute()['items'] 
+        result = service.playlistItems().list(
+                                              part='contentDetails',
+                                              playlistId=playlist_id,
+                                              maxResults=50
+                                             ).execute()['items']
         return [item['contentDetails']['videoId'] for item in result]
