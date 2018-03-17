3805dd597c785ef46d9511a3c2d76d400c438393
cleviojr <cleviojr20@gmail.com>
2018-03-16 22:39:29 -0300

    fix spaces for PEP8

--- akane.py
+++ akane.py
@@ -1,9 +1,10 @@
 import asyncio
 
-from discord            import Client, opus
-from main_listener      import MainListener
-from config             import CONFIG
-from music.manager      import Manager
+from discord import Client, opus
+from main_listener import MainListener
+from config import CONFIG
+from music.manager import Manager
+
 
 class Akane():
     def __init__(self):
@@ -11,8 +12,8 @@ class Akane():
         MainListener(self.client, Manager())
 
         opus.load_opus('libopus-0.x86.dll')
-        #libs: 'libopus-0.x86.dll', 'libopus-0.x64.dll',
-        #'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib'.
+        # libs: 'libopus-0.x86.dll', 'libopus-0.x64.dll',
+        # 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib'.
 
         loop = asyncio.get_event_loop()
         try:
