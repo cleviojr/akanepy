b13a69602d6bbb085d0dbea030449107d8d57798
cleviojr <cleviojr20@gmail.com>
2018-03-16 22:41:47 -0300

    fix PEP8, remove !ping and !vol, add !setmusic, rename functions

--- commands/utils/core.py
+++ commands/utils/core.py
@@ -1,54 +1,55 @@
 from commands.utils.parser import parse
 
-#general commands
-from commands.ping import PingCmd
+# music commands
+from commands.music.play import PlayCmd
+from commands.music.skip import SkipCmd
+from commands.music.shuffle import ShuffleCmd
+from commands.music.clear import ClearCmd
+from commands.music.setmusic import SetMusicCmd
 
-#music commands
-from commands.play    import PlayCmd
-from commands.skip    import SkipCmd
-from commands.vol     import VolCmd
-from commands.shuffle import ShuffleCmd
-from commands.clear   import ClearCmd
-
-GENERAL_COMMAND_LIST = {
-    '!ping': PingCmd,
-}
+# search commands
+from commands.search.nyaa import NyaaCmd
 
 MUSIC_COMMAND_LIST = {
     '!play': PlayCmd,
     '!skip': SkipCmd,
-    '!vol': VolCmd,
     '!shuffle': ShuffleCmd,
     '!clear': ClearCmd,
+    '!setmusic': SetMusicCmd,
 }
+
+SEARCH_COMMAND_LIST = {
+    '!nyaa': NyaaCmd,
+}
+
 
 class Runner():
     def __init__(self, client, message, manager):
         self.message = message
-        self.client  = client
+        self.client = client
         self.manager = manager
-            
+
         self.key, self.args = parse(message)
 
-    def is_command(self):
-        for j in GENERAL_COMMAND_LIST.keys(): 
-            if self.key == j: 
-                return True 
-
     def is_music_command(self):
-        for j in MUSIC_COMMAND_LIST.keys(): 
-            if self.key == j: 
+        for j in MUSIC_COMMAND_LIST.keys():
+            if self.key == j:
+                return True
+
+    def is_search_command(self):
+        for j in SEARCH_COMMAND_LIST.keys():
+            if self.key == j:
                 return True
 
     async def run(self):
         command = None
-        if self.is_command(): 
-            command = GENERAL_COMMAND_LIST[self.key](self.client)
-        elif self.is_music_command(): 
+        if self.is_music_command():
             command = MUSIC_COMMAND_LIST[self.key](self.client, self.manager)
+        elif self.is_search_command():
+            command = SEARCH_COMMAND_LIST[self.key](self.client)
 
         if command:
             await command.run(self.message, self.args)
             print(f'{self.key} with success.')
         else:
-            print('It was not a command.')            
+            print('It was not a command.')
